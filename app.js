ffb152b4271c63a22b9b6eb7ac9d967712db790d
AriadnaBelenM <117781528+AriadnaBelenM@users.noreply.github.com>
2023-01-07 17:48:13 -0600

    nuevo app.js

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
 const express = require('express');
-const path = require('path')
 const app = express();
+const path = require('path');
+const mainController = require('./controllers/mainController');
+const router = require('./routes/mainRouter');
+const mainRouter = require ("./routes/mainRouter");
 const publicPath = path.resolve(__dirname, './public')
-app.use(express.static(publicPath) )
+
+app.use(express.static("publicPath"))
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.use(mainRouter);
 
 app.listen(3010, () => 
 console.log('Servidor funcionando')
 )
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/otroPosibleHome.html'))
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/register.html'))
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/login.html'))
-});
-
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/productDetail.html'))
-});
-
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/productCart.html'))
-});
-
-app.get('/iphone13', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/iphone13.html'))
-});
-
-app.get('/s22ultra', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/s22ultra.html'))
-});
-
-app.get('/s21fe', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './/views/s21fe.html'))
-});
