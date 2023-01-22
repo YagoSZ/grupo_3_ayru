8b93afabcdf34c2c69ba221cfdc347df883d8690
AriadnaBelenM <117781528+AriadnaBelenM@users.noreply.github.com>
2023-01-22 14:45:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const mainController = require('./controllers/mainController');
 const router = require('./routes/mainRouter');
 const mainRouter = require ("./routes/mainRouter");
 const publicPath = path.resolve(__dirname, './public')
+const methodOverride = require('method-override')
 
 app.use(express.static("public"))
-
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
