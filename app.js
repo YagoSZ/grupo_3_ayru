dd7792f13ef2178dcf1362deae47704ab32e280f
AriadnaBelenM <117781528+AriadnaBelenM@users.noreply.github.com>
2023-01-23 20:21:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const multer = require('multer')
-const uploadFile = multer({storage})
-const mainController = require('./controllers/mainController');
+// const uploadFile = multer({storage})
+// const mainController = require('./controllers/mainController');
 const router = require('./routes/mainRouter');
 const mainRouter = require ("./routes/mainRouter");
 const publicPath = path.resolve(__dirname, './public')
 const methodOverride = require('method-override')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
 app.use(express.static("public"))
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
