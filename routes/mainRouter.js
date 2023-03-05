defab7ff94d0b960a3c787631dff67737c95985b
AriadnaBelenM <117781528+AriadnaBelenM@users.noreply.github.com>
2023-03-05 15:36:45 -0300

    multer

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const multer = require('multer')
+const path = require("path")
 const mainController = require('../controllers/mainController')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../public/img/users"))
+    },
+    filename: (req, file, cb) => {
+        const newFileName = "usuario-" + Date.now() + path.extname(file.originalname)
+cb(null, newFileName)
+}
+})
+
+const upload = multer ({ storage})
 
 router.get('/', mainController.index);
 
 router.get('/register', mainController.register);
+router.post('/register', upload.single("imagenUsuario"), mainController.register);
 
 router.get('/login', mainController.login);
 
