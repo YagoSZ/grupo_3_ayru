f0a48fc012d131ef54b1c35485cd7617f279834e
AriadnaBelenM <ariadnabelen97@gmail.com>
2023-05-03 19:53:30 -0300

    react header

--- react-vite/src/components/App.jsx
+++ react-vite/src/components/App.jsx
@@ -1,41 +1,11 @@
 // import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
+import Header from './Header.jsx';
+
 
 function App(){
 return (
     <>
-    <h1 className='text-center text-black bg-secondary p-4'>Bienvenidos</h1>
-    <Carousel slide={false}>
-      <Carousel.Item>
-        <img style={{ width : "25rem" }}
-          className="d-block w-0"
-          src="../../img/s22verde.png"
-        />
-        <Carousel.Caption>
-          <h3 className='text-black'>First slide</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img style={{ width : "18rem"  }}
-          className="d-block w-0"
-          src="../../img/s21fe-dorado.jpg"
-        />
-
-        <Carousel.Caption>
-          <h3 className='text-black'>Second slide</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img style={{ width : "18rem"  }}
-          className="d-block w-0"
-          src="../../img/iphone13pro-azul.jpg"
-        />
-
-        <Carousel.Caption>
-          <h3 className='text-black'>Third slide</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+       <Header />
     </>
 )
 
---
+++ react-vite/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">AYRU</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Products</Nav.Link>
+              <Nav.Link href="#features">Contact</Nav.Link>
+              <Nav.Link href="#pricing">Info</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+  
+  export default Header;
