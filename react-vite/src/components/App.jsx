21fb91396f9bf358b920ff3a8166faf7cdb9b091
AriadnaBelenM <117781528+AriadnaBelenM@users.noreply.github.com>
2023-05-07 13:12:43 -0300

    terminar react

--- react-vite/src/components/App.jsx
+++ react-vite/src/components/App.jsx
@@ -1,12 +1,18 @@
 import '../assets/css/styles.css';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+import UsersList from './UsersList';
+import Products from './Products';
+import UltimoUsuario from './UltimoUsuario';
 
 
 function App(){
 return (
     <>
        <Header />
+        <Products />
+       <UsersList />
+       <UltimoUsuario />
        <Footer />
     </>
 
